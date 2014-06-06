bd96e51313cce16ac4c6eefdab4ddd5780150b1d
Ha Anh Son <hason61vn@gmail.com>
2014-06-06 18:12:19 +0700

    update admin

---
+++ www/protected/modules/admin/controllers/CrawlerController.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Created by SonHA
+ * User: Son Ha Anh (sonhaanh@vccorp.vn)
+ * Date: 6/6/14
+ * Time: 6:10 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class NewsController extends Controller
+{
+    /**
+     * Todo: Active craler tu bao dantri
+     * Author: Son Ha Anh (sonhaanh@vccorp.vn)
+     * Create:
+     * Update:
+     * Output: None
+     */
+    public function actionCrawlerDantri(){
+
+    }
+}
