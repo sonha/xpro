f3380f5250b78c922064c2c522d5d55ebd1596ae
Ha Anh Son <hason61vn@gmail.com>
2014-07-16 13:55:15 +0700

    Change Rewrite Url

---
+++ www/protected/controllers/UserController.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by SonHA
+ * User: Son Ha Anh (sonhaanh@vccorp.vn)
+ * Date: 7/16/14
+ * Time: 1:43 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class UserController extends Controller{
+
+    /**
+     * Todo: Ham list toan bo User
+     * Author: Son Ha Anh (sonhaanh@vccorp.vn)
+     * Create:
+     * Update:
+     * Output: None
+     * Link : http://localhost/xpro/www/index.php?r=user/listUser
+     */
+    public function actionListUser() {
+        $this->render('list');
+
+    }
+}
---
+++ www/protected/views/user/list.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by SonHA
+ * User: Son Ha Anh (sonhaanh@vccorp.vn)
+ * Date: 7/16/14
+ * Time: 1:44 PM
+ * To change this template use File | Settings | File Templates.
+ */ 
+?>
+<table>
+    <tr>
+        <td>ID</td>
+        <td>Username</td>
+        <td>Email</td>
+        <td>Mobile</td>
+        <td>Facebook</td>
+        <td>Address</td>
+    </tr>
+</table>
