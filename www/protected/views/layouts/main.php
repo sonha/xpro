70c2c1ca3513221aed6845d407a9ba061b086b1d
Ha Anh Son <hason61vn@gmail.com>
2014-07-19 10:45:54 +0700

    Update ErrorController

--- www/protected/views/layouts/main.php
+++ www/protected/views/layouts/main.php
@@ -33,6 +33,7 @@
 				array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
 				array('label'=>'Contact', 'url'=>array('/site/contact')),
 				array('label'=>'Bài giảng', 'url'=>array('/article/index')),
+				array('label'=>'Sản phẩm', 'url'=>array('/product/index')),
                 array('label'=>'User', 'url'=>array('/user/listUser')),
 				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
 				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
